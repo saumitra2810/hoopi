4bf29594f7b2bc894cf072a4d720c1044e8fefd5
saumitra1993 <theapogee2011@gmail.com>
2019-05-13 16:24:51 +0530

    added xml html

--- astronaut/routes/index.js
+++ astronaut/routes/index.js
@@ -63,7 +63,8 @@ function buildOembedResponse(url, maxWidth, maxHeight){
   var response = {
     "version":"1.0",
     "type":"rich",
-    "html":"<iframe width=\"" + maxWidth + "\" height=\"" + maxHeight + "\" src=\"" + url + "\" frameborder=\"0\" ><\/iframe>",
+    "html": `<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/strict.dtd"> <html xmlns="http://www.w3.org/TR/xhtml1/strict" ><head><title>Shopping list</title> </head><body>
+  ` + "<iframe width=\"" + maxWidth + "\" height=\"" + maxHeight + "\" src=\"" + url + "\" frameborder=\"0\" ><\/iframe></body></html>",
     "width": maxWidth,
     "height": maxHeight,
     "author_name":"Arvind Krishnan",
